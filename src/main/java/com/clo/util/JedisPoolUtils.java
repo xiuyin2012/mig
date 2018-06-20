de0c8ce06b75b3b62beb6a4bf62b6e5b916b0500
liut <xiuyin2012@126.com>
2018-06-20 07:25:03 -0700

    add forward initPage

--- src/main/java/com/clo/util/JedisPoolUtils.java
+++ src/main/java/com/clo/util/JedisPoolUtils.java
@@ -7,6 +7,7 @@ package com.clo.util;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
+import redis.clients.jedis.exceptions.JedisConnectionException;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -15,7 +16,8 @@ import javax.naming.NamingException;
 public class JedisPoolUtils {
 
     private static JedisPool pool;
-
+    static String Auth = null;
+    static String URL = "";
     /**
      * 建立连接池 真实环境，一般把配置参数缺抽取出来。
      *
@@ -30,18 +32,26 @@ public class JedisPoolUtils {
         config.setMaxTotal(100);
         // 设置最大阻塞时间，记住是毫秒数milliseconds
         config.setMaxWaitMillis(10*1000);
-        // 设置空间连接
+        // 设置连接
         config.setMaxIdle(10);
+
+        //在borrow(引入)一个jedis实例时，是否提前进行validate操作；如果为true，则得到的jedis实例均是可用的；
+        //config.setTestOnBorrow(true);
+        //return 一个jedis实例给pool时，是否检查连接可用性（ping()）
+        //config.setTestOnReturn(true);
         Context ctx = null;
-        String url = null;
+        //String url = null;
         try {
             ctx = (Context) new InitialContext().lookup("java:comp/env");
-            url = (String) ctx.lookup("redisUrl");
+            URL = (String) ctx.lookup("redisUrl");
         } catch (NamingException e) {
             e.printStackTrace();
+
         }
         // 创建连接池
-        pool = new JedisPool(config, url.split(":")[0],Integer.valueOf(url.split(":")[1]));
+        String[] urlArray =  URL.split(":");
+        if(urlArray.length==3)Auth = urlArray[2];
+        pool = new JedisPool(config, urlArray[0],Integer.valueOf(urlArray[1]),10*1000,Auth);
 
     }
 
@@ -59,10 +69,22 @@ public class JedisPoolUtils {
      * @return
      */
     public static Jedis getJedis() {
-
-        if (pool == null)
-            poolInit();
-        return pool.getResource();
+        if (pool == null) poolInit();
+        Jedis jedis = null;
+        boolean exceptionflag = true;
+        try{
+            jedis = pool.getResource();
+        }catch (JedisConnectionException e){
+            e.printStackTrace();
+            exceptionflag = false;
+/*            if (jedis != null)
+                pool.returnBrokenResource(jedis);*/
+        }finally {
+            //bizClass have closed jedis through 'jedis.close',so cancel here
+/*            if (exceptionflag)
+                pool.returnResourceObject(jedis);*/
+        }
+        return jedis;
     }
 
     /**
